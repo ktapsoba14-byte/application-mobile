f7e74feff5685b23f2fa124ca2e7a586b9293879
Kevin Tapsoba <ktapsoba14@gmail.com>
2026-02-19 20:25:49 +0000

    Page lavande.dart en cours d'optimisation

--- lib/pages/aromatiques/lavande.dart
+++ lib/pages/aromatiques/lavande.dart
@@ -8,10 +8,203 @@ class Lavande extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Retour aux Plantes Aromatique',
+          'Retour aux Plantes Aromatiques',
           style: TextStyle(
             color: Colors.green,
             fontStyle: FontStyle.italic,
+          ),
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.green.shade50,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Container(
+                  width: 450,
+                  height: 450,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: const BorderRadius.vertical(
+                            top: Radius.circular(13)),
+                        child: Image.asset(
+                          'assets/images/lavande.jpeg',
+                          width: 450,
+                          height: 250,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Row(
+                              children: [
+                                Icon(Icons.eco, color: Colors.green),
+                                SizedBox(width: 5),
+                                Text(
+                                  "PLANTE MÉDICINALE",
+                                  style: TextStyle(
+                                      color: Colors.green,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 15),
+                            const Text(
+                              "Lavande",
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const Text(
+                              "Lavandula angustifolia",
+                              style: TextStyle(
+                                fontStyle: FontStyle.italic,
+                                fontSize: 20,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            const SizedBox(height: 15),
+                            const Text(
+                              "Plante aromatique aux propriétés calmantes et relaxantes.",
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Container(
+                  width: 450,
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.05),
+                        blurRadius: 8,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.check_circle_outline,
+                            color: Colors.green,
+                            size: 30,
+                          ),
+                          SizedBox(
+                              width: 10),
+                          Text(
+                            "Vertus et Bienfaits",
+                            style: TextStyle(color: Colors.green, fontSize: 25),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 15),
+                      Row(
+                        children: [
+                          Icon(Icons.circle, color: Colors.green, size: 10),
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: Text(
+                                "Favorise la relaxation et réduit le stress",
+                                style: TextStyle(fontSize: 16)),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 15),
+                      Row(
+                        children: [
+                          Icon(Icons.circle, color: Colors.green, size: 10),
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: Text("Améliore la qualité du sommeil",
+                                style: TextStyle(fontSize: 16)),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 15),
+                      Row(
+                        children: [
+                          Icon(Icons.circle, color: Colors.green, size: 10),
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: Text(
+                                "Soulage les maux de tête et de migraines",
+                                style: TextStyle(fontSize: 16)),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 15),
+                      Row(
+                        children: [
+                          Icon(Icons.circle, color: Colors.green, size: 10),
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: Text(
+                              "Possède des propriétés antiseptiques et cicatrisantes",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 15),
+                      Row(
+                        children: [
+                          Icon(Icons.circle, color: Colors.green, size: 10),
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: Text(
+                                "Aide à apaiser les irritations cutanées",
+                                style: TextStyle(fontSize: 16)),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Container(
+                  width: 450,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.05),
+                        blurRadius: 8,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  
+                ),
+              )
+            ],
           ),
         ),
       ),
